e072c9b4b22da6f4b4a04c58a945fc84a811a54a
alejandro-montepeque <jonathanmontepeque@gmail.com>
2025-06-08 23:27:19 -0600

    add: sweet alert

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap';
+import Swal from 'sweetalert2';
+
+// Mostrar la alerta al cargar la página
+if (document.querySelector('h2')?.textContent.includes('Calculadora Básica')) {
+  Swal.fire({
+    title: '¡Bienvenido a mi calculadora web!',
+    text: 'Ingresa los números que deseas operar y luego selecciona la operación a realizar.',
+    icon: 'info',
+    confirmButtonText: 'Entendido'
+  });
+}
+
 
  // Se hace una petición para obtener el archivo 'navbarComponent.html'y donde se encuentra ubicado
     fetch('/components/navbarComponent.html')
